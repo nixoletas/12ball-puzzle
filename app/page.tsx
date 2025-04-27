b43e83f82f254ad3588e4585dc4e3e7a0ec353aa
nixoletas <nicholetas@gmail.com>
2025-04-27 08:02:10 -0400

    refactor: update font family in globals.css and clean up page.tsx layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import GameBoard from "@/components/game-board";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2 md:p-4">
+      <div className="z-10 w-full max-w-5xl items-center justify-between text-sm">
         <h1 className="mb-4 text-center text-3xl font-bold">
           The 12 Balls Puzzle
         </h1>
         <p className="mb-8 text-center">
-          One of these 12 balls has a different weight. Can you find it in just
-          3 weighings?
+          One of these 12 balls has a different weight. Your goal is to find
+          that ball with only 3 weighings?
         </p>
         <GameBoard />
       </div>
